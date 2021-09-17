e00e10cc9e54f2bbf554c554138dd6b2d823db7f
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-09-17 20:47:02 +1000

    start()

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    int Vertical, Horizontal; 
+    // [SerializeField] private GameObject tile; 
+    // [SerializeField] private Sprites[] sprites; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Vertical = (int)Camera.main.orthographicSize; 
+        Horizontal = Vertical * (Screen.width/ Screen.height); 
 
-    // Update is called once per frame
-    void Update()
-    {
         int[,] levelMap = 
         { 
         {1,2,2,2,2,2,2,2,2,2,2,2,2,7}, 
@@ -36,29 +37,70 @@ public class LevelGenerator : MonoBehaviour
         for(int i = 0; i < levelMap.GetLength(0); i++){
             // j = column
             for(int j = 0; j < levelMap.GetLength(1); j++){
+
+                switch(levelMap[i,j]){
+                    case 0:
+                        Debug.Log("Nothing printed");
+                    break; 
+                    case 1:
+                        Debug.Log(string.Format("an Outside Corner tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    case 2: 
+                        Debug.Log(string.Format("an Outside Wall tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    case 3:
+                        Debug.Log(string.Format("an Inside Corner tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    case 4:
+                        Debug.Log(string.Format("an Inside Wall tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    case 5: 
+                        Debug.Log(string.Format("a Standard Pellet tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    case 6: 
+                        Debug.Log(string.Format("a Power Pellet tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    case 7: 
+                        Debug.Log(string.Format("a T-Junction tile was drawn at {0} and {1}", i , j));
+                    break; 
+                    default:
+                    break; 
+
+                }
                 
-                if(levelMap[i, j] == 1){
-                    Debug.Log(string.Format("an Outside Corner tile was drawn at {0} and {1}", i , j));
-                }
-                else if(levelMap[i, j] == 2){
-                    Debug.Log(string.Format("an Outside Wall tile was drawn at {0} and {1}", i , j));
-                }
-                else if(levelMap[i, j] == 3){
-                     Debug.Log(string.Format("an Inside Corner tile was drawn at {0} and {1}", i , j));
-                }
-                else if(levelMap[i, j] == 4){
-                    Debug.Log(string.Format("an Inside Wall tile was drawn at {0} and {1}", i , j));
-                }
-                else if(levelMap[i, j] == 5){
-                    Debug.Log(string.Format("a Standard Pellet tile was drawn at {0} and {1}", i , j));
-                }
-                else if(levelMap[i, j] == 6){
-                    Debug.Log(string.Format("a Power Pellet tile was drawn at {0} and {1}", i , j));
-                }
-                else if(levelMap[i, j] == 7){
-                    Debug.Log(string.Format("a T-Junction tile was drawn at {0} and {1}", i , j));
-                }
+                // if(levelMap[i, j] == 1){
+                //     Debug.Log(string.Format("an Outside Corner tile was drawn at {0} and {1}", i , j));
+                //     Instantiate(one);
+                // }
+                // else if(levelMap[i, j] == 2){
+                //     Debug.Log(string.Format("an Outside Wall tile was drawn at {0} and {1}", i , j));
+                // }
+                // else if(levelMap[i, j] == 3){
+                //      Debug.Log(string.Format("an Inside Corner tile was drawn at {0} and {1}", i , j));
+                // }
+                // else if(levelMap[i, j] == 4){
+                //     Debug.Log(string.Format("an Inside Wall tile was drawn at {0} and {1}", i , j));
+                // }
+                // else if(levelMap[i, j] == 5){
+                //     Debug.Log(string.Format("a Standard Pellet tile was drawn at {0} and {1}", i , j));
+                // }
+                // else if(levelMap[i, j] == 6){
+                //     Debug.Log(string.Format("a Power Pellet tile was drawn at {0} and {1}", i , j));
+                // }
+                // else if(levelMap[i, j] == 7){
+                //     Debug.Log(string.Format("a T-Junction tile was drawn at {0} and {1}", i , j));
+                // }
             }
         }
+    }
+
+    // private Vector3 GridToWorldPosition(int x, int y){
+    //     return new Vector3()
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }

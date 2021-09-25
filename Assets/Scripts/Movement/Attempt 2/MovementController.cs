7000ef3e56a56ce85442d98fc487fff964a58701
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-09-26 01:42:18 +1000

    bool

--- Assets/Scripts/Movement/Attempt 2/MovementController.cs
+++ Assets/Scripts/Movement/Attempt 2/MovementController.cs
@@ -8,22 +8,27 @@ public class MovementController : MonoBehaviour
     public GameObject obj; 
     public Transform[] targets; 
 
-    public Movement movement; 
-    //private bool hasReached = false; 
+    //public Movement movement; 
+    
+    private bool rightTurn = false; 
+    private bool leftTurn = false; 
+    private bool up = false; 
+    private bool down = false; 
+
     public Animator animator; 
 
     // Start is called before the first frame update
     void Start()
     {
         //tweener = GetComponent<Tweener>();
-        movement = GetComponent<Movement>(); 
+        //movement = GetComponent<Movement>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         if(obj.transform.position == targets[0].transform.position){
-            
+
         }
         if(obj.transform.position == targets[1].transform.position){
 

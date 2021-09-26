ce97dfdef8c17d443cc5c30c736746d09e378b6a
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-09-26 20:39:34 +1000

    Touch-Ups

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip loopAudio; 
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+
+        audio.Play(); 
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = loopAudio; 
+        audio.loop = true; 
+        audio.Play(); 
+    }
+
+
+}

84687174659e591c45b452ea80bf4e39491df720
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-11-04 11:46:46 +1100

    Cherry-Spawn

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CherryController : MonoBehaviour
 {
+    public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine("CherrySpawn");
+    }
+
+    IEnumerator CherrySpawn()
+    {
+        while (true)
+        {
+            Instantiate(prefab);
+            yield return new WaitForSeconds(10.0f);
+        }
+
     }
 }

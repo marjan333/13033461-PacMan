d41c203905a246bea69adc6626ffe8d165228c00
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-11-04 23:27:41 +1100

    Update CherryController.cs

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CherryController : MonoBehaviour
 {
+    private float minX, maxX, minY, maxY;
     private float minCam;
     private float maxCam;
     private Vector3 spawnPos;
@@ -11,16 +12,21 @@ public class CherryController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameObject mainSceneCam = GameObject.Find("Camera");
+        Camera cam = mainSceneCam.GetComponent<Camera>();
+        Transform camPos = cam.transform;
+
         // minCam = Camera.main.orthographicSize;
         // maxCam = Camera.main.orthographicSize + 2;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(minCam);
-        spawnPos.x = Random.Range(-45, 78);
-        spawnPos.y = Random.Range(-53, 12);
+        spawnPos.x = Random.Range(minX, maxX);
+        spawnPos.y = Random.Range(minY, maxY);
         spawnPos.z = 0;
         StartCoroutine("CherrySpawn");
     }
@@ -28,12 +34,9 @@ public class CherryController : MonoBehaviour
     IEnumerator CherrySpawn()
     {
 
+        Instantiate(prefab, new Vector3(spawnPos.x, spawnPos.y, spawnPos.z), Quaternion.identity);
+        yield return new WaitForSeconds(10.0f);
 
-        while (true)
-        {
-            Instantiate(prefab, new Vector3(spawnPos.x, spawnPos.y, spawnPos.z), Quaternion.identity);
-            yield return new WaitForSeconds(10.0f);
-        }
 
     }
 }

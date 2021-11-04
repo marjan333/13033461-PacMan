7319e7ad0b1a2b3fa269a38d80c09a824a64d214
marjan333 <37725791+marjan333@users.noreply.github.com>
2021-11-04 21:52:35 +1100

    inside camera

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class CherryController : MonoBehaviour
 {
-    private Vector3 CherryPos = new Vector3(-45, -19, 0);
+    private float minCam;
+    private float maxCam;
+    private Vector3 spawnPos;
     public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
-
+        // minCam = Camera.main.orthographicSize;
+        // maxCam = Camera.main.orthographicSize + 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(minCam);
+        spawnPos.x = Random.Range(-45, 78);
+        spawnPos.y = Random.Range(-53, 12);
+        spawnPos.z = 0;
         StartCoroutine("CherrySpawn");
     }
 
     IEnumerator CherrySpawn()
     {
+
+
         while (true)
         {
-            Instantiate(prefab, CherryPos, Quaternion.identity);
+            Instantiate(prefab, new Vector3(spawnPos.x, spawnPos.y, spawnPos.z), Quaternion.identity);
             yield return new WaitForSeconds(10.0f);
         }
 
